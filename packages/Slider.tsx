f9b69e89a7a14f7f53adb2473079fcc34867c66c
arsrna <root@arsrna.cn>
2025-09-13 22:10:28 +0800

    feat(animate): 完善动画

--- packages/Slider.tsx
+++ packages/Slider.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useEffect, useState } from 'react';
+import { CSSProperties, useEffect, useRef, useState } from 'react';
 import style from './index.module.scss';
 import { itemProps } from './type';
 
@@ -6,15 +6,33 @@ import leftIcon from './assets/left.svg';
 
 export default function Slider(props: {
     item: itemProps[],
+    /**container样式 */
     style?: CSSProperties;
     /**每一个轮播项container的样式 */
     itemStyle?: CSSProperties;
     /**是否启用背景图片 */
     backgroundImage?: boolean;
+    /**是否自动播放 */
+    autoplay?: boolean;
+    /**自动播放间隔(ms)，建议不小于3秒，否则动画无法正常播放 */
+    interval?: number;
+    /**在hover时停止轮播 */
+    pauseOnHover?: boolean;
 }) {
-    const { item, itemStyle, backgroundImage } = props;
+    const { item, itemStyle, backgroundImage = true, autoplay = true, interval = 5000, pauseOnHover = true } = props;
 
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    const timer = useRef(null);
+
+    useEffect(() => {
+        if (autoplay) {
+            handleStartInterval();
+        }
+        return () => {
+            handleStopInterval();
+        };
+    }, [autoplay, interval]);
 
     useEffect(() => {
         item.forEach((m) => {
@@ -23,23 +41,42 @@ export default function Slider(props: {
         });
     }, [item]);
 
+    function handleStartInterval() {
+        // console.log('startInterval');
+        if (timer.current !== null) return;
+        timer.current = setInterval(() => {
+            handleRight();
+        }, interval);
+    }
+
+    function handleStopInterval() {
+        // console.log('stopInterval');
+        if (timer.current === null) return;
+        clearInterval(timer.current);
+        timer.current = null;
+    }
+
     function handleLeft() {
-        if (currentIndex === 0) {
-            setCurrentIndex(item.length - 1);
-        } else {
-            setCurrentIndex(currentIndex - 1);
-        }
+        setCurrentIndex(prevIndex => {
+            const newIndex = prevIndex === 0 ? item.length - 1 : prevIndex - 1;
+            return newIndex;
+        });
     }
 
     function handleRight() {
-        if (currentIndex === item.length - 1) {
-            setCurrentIndex(0);
-        } else {
-            setCurrentIndex(currentIndex + 1);
-        }
+        setCurrentIndex(prevIndex => {
+            const newIndex = prevIndex === item.length - 1 ? 0 : prevIndex + 1;
+            return newIndex;
+        });
     }
 
-    return (<><div className={style["slider-container"]} style={props.style || { height: '60vh' }}>
+    return (<div className={style["slider-container"]} style={props.style || { height: '60vh' }}
+        onMouseLeave={() => {
+            if (pauseOnHover) handleStartInterval();
+        }}
+        onMouseEnter={() => {
+            if (pauseOnHover) handleStopInterval();
+        }}>
         <div className={style["slider-handler"]}>
             <img src={leftIcon} onClick={handleLeft} />
             <img src={leftIcon} style={{ transform: 'rotate(180deg)' }} onClick={handleRight} />
@@ -66,5 +103,5 @@ export default function Slider(props: {
         </div>
         <div className={style["slider-dot"]}>{item && item.map((m, i) => (<div>{i}</div>))}</div>
     </div>
-    </>);
+    );
 }
